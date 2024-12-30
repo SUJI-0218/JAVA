e33638e552725160b795f547664d5ec7742fe448
김수지 <zzz2134@naver.com>
2024-12-30 12:37:50 +0900

    abcd

---
+++ src/members_/MemberClass.java
@@ -0,0 +1,13 @@
+package members_;
+
+public class MemberClass {
+	public String name, addr;
+	public void allprint() {
+		System.out.println("이름 : " + name );
+		System.out.println("주소 : " + addr );
+		
+		//하나의 기능을 만들어 둠
+		
+		
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module abcd {
+	exports members_;
+	//exports는 파일을 내보내는 역할
+	
+}
